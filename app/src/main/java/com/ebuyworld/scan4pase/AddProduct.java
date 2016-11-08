5c46a206a02763bc9717c01cd0370f130ddaee15
Dhruv Sringari <dhruvy427@gmail.com>
2016-11-08 13:05:37 -0500

    Adds activity

---
+++ app/src/main/java/com/ebuyworld/scan4pase/AddProduct.java
@@ -0,0 +1,21 @@
+package com.ebuyworld.scan4pase;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class AddProduct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_product);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
